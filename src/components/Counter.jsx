abd3564c3cccd857629cf02bba79eccf9f7d4c9b
RAHUL <rahulmeetawa@gmail.com>
2023-06-26 16:12:06 +0530

    add hooks in counter componentss

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
-  return <div></div>;
+  const [counter, setCounter] = useState(1);
+  const Increment = () => {
+    setCounter(Counter + 1);
+  };
+  const Decrement = () => {
+    setCounter(Counter - 1);
+  };
+  return (
+    <>
+      <h1>State is : {counter}</h1>
+      <button onClick={Decrement}>-</button>
+      <button onClick={Increment}>+</button>
+    </>
+  );
 };
 
 export default Counter;
