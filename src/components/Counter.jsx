479270c6cb87bf5371a83d73954599dc190e8836
RAHUL <rahulmeetawa@gmail.com>
2023-06-26 16:46:30 +0530

    add new initial value

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const Counter = () => {
-  const [counter, setCounter] = useState(1);
+  const [counter, setCounter] = useState(0);
   const Increment = () => {
     return setCounter(counter + 1);
   };
